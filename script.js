65d54593c577750b2712e043d06ceaf815d0cb4d
soadaa <Soadhlh@gmail.com>
2022-07-07 16:19:17 -0400

    Start of operate function
    I figured out how to split the string by the operate. I believe my next best step is to figure out how to evulate an equation in reverse polish notation. Then make my string fit into that format to be solved

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandElement = document.querySelector('[data-previous-operand]');
 const currentOperandElement = document.querySelector('[data-current-operand]');
 
+let operandPosistion;
 const equationArray = [];
 
 for (i = 0; i < numberButtons.length; i++) {
@@ -23,15 +24,23 @@ for (i = 0; i < operationButtons.length; i++) {
         let operand = currentlyClickedButton.getAttribute('data-operation');
         currentOperandElement.textContent += ' ' + operand + ' ';
         equationArray.push(operand);
+        operandPosistion = equationArray.indexOf(operand);
     });
 }
 
 equalButton.addEventListener('click', () => {
     let result = ''
+    operate();
     previousOperandElement.textContent = currentOperandElement.textContent + ' = ' + result;
     currentOperandElement.textContent = result;
-    console.log(equationArray);
+    equationArray.length = 0;
 });
+
+const operate = function() {
+    let temp = equationArray.toString().replace(/,/g,"");
+    temp = temp.slice(0, operandPosistion + 1);
+    console.log(temp);
+}
 
 const sum = function(numbers) {
     let total = 0;
@@ -61,3 +70,4 @@ const power = function(a,b) {
 	let total = Math.pow(a,b);
   return total;
 };
+
