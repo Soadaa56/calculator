02528e33ac3dccd90187f4fc6ca62ea073983124
soadaa <Soadhlh@gmail.com>
2022-07-07 11:31:25 -0400

    Data-number values and EventListener on buttons
    The event listeners on the buttons push the values on data-number and data-operand to equationArray and input-display. The equal button displays total result (which still requires a function) and put previous equation into top output-display.

--- script.js
+++ script.js
@@ -5,6 +5,34 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandElement = document.querySelector('[data-previous-operand]');
 const currentOperandElement = document.querySelector('[data-current-operand]');
+
+const equationArray = [];
+
+for (i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', function(e) {
+        let currentlyClickedButton = e.target;
+        let value = currentlyClickedButton.getAttribute('data-number');
+        currentOperandElement.textContent += value;
+        equationArray.push(value);
+    });
+}
+
+for (i = 0; i < operationButtons.length; i++) {
+    operationButtons[i].addEventListener('click', function(e) {
+        let currentlyClickedButton = e.target;
+        let operand = currentlyClickedButton.getAttribute('data-operation');
+        currentOperandElement.textContent += ' ' + operand + ' ';
+        equationArray.push(operand);
+    });
+}
+
+equalButton.addEventListener('click', () => {
+    let result = ''
+    previousOperandElement.textContent = currentOperandElement.textContent + ' = ' + result;
+    currentOperandElement.textContent = result;
+    console.log(equationArray);
+});
+
 const sum = function(numbers) {
     let total = 0;
     for (i = 0; i < numbers.length; i++) {
@@ -27,16 +55,9 @@ const divide = function(numbers) {
         total /= numbers[i];
     }
     return total;
-}
+};
 
 const power = function(a,b) {
 	let total = Math.pow(a,b);
   return total;
 };
-
-function operator() {
-    operator = {
-        sum, 
-        multiply,
-    }
-}
