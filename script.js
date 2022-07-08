90d0cf4f86abc553e833fe5167d7dc40f3279875
soadaa <Soadhlh@gmail.com>
2022-07-08 16:56:48 -0400

    Clear button, operate function on equals, getEquationResults function
    The calculator now has a functioning all clear button. The calculator will now do the 4 basic operands on two intergers; decimals are ignored in the calculation.

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandElement = document.querySelector('[data-previous-operand]');
 const currentOperandElement = document.querySelector('[data-current-operand]');
 
-let operandPosistion;
 const equationArray = [];
+let operandPosistion;
+let equationResult;
 
+// numberButtons
 for (i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', function(e) {
         let currentlyClickedButton = e.target;
@@ -18,6 +20,7 @@ for (i = 0; i < numberButtons.length; i++) {
     });
 }
 
+// operands
 for (i = 0; i < operationButtons.length; i++) {
     operationButtons[i].addEventListener('click', function(e) {
         let currentlyClickedButton = e.target;
@@ -29,19 +32,52 @@ for (i = 0; i < operationButtons.length; i++) {
 }
 
 equalButton.addEventListener('click', () => {
-    let result = ''
     operate();
-    previousOperandElement.textContent = currentOperandElement.textContent + ' = ' + result;
-    currentOperandElement.textContent = result;
+    previousOperandElement.textContent = currentOperandElement.textContent + ' = ' + equationResult;
+    currentOperandElement.textContent = equationResult;
     equationArray.length = 0;
 });
 
+allClearButton.addEventListener('click', () => {
+    equationArray.length = 0;
+    operandPosistion = '';
+    equationResult = '';
+    previousOperandElement.textContent = '';
+    currentOperandElement.textContent = '';
+});
+
 const operate = function() {
-    let temp = equationArray.toString().replace(/,/g,"");
-    temp = temp.slice(0, operandPosistion + 1);
-    console.log(temp);
+    // Allows array to be sliced; Combines multiple numbers into a singular term
+    let expression = equationArray.toString().replace(/,/g,"");
+
+    // Seperates the two terms from the operator
+    termOne = parseInt(expression.slice(0, operandPosistion));
+    termTwo = parseInt(expression.slice(operandPosistion + 1));
+    operand = expression.charAt(operandPosistion);
+    getEquationResult(operand);
 }
 
+
+const getEquationResult = function(operand) {
+    switch (operand) {
+        case '+':
+            equationResult = termOne + termTwo;
+            break;
+        case '-':
+            equationResult = termOne - termTwo;
+            break;
+        case 'x' :
+            equationResult = termOne * termTwo;
+            break;
+        case '/' :
+            equationResult = termOne / termTwo;
+            break;
+        default:
+            console.log('bug on switch statement')
+            equationResult = 'error'
+            break;
+    }
+}
 const sum = function(numbers) {
     let total = 0;
     for (i = 0; i < numbers.length; i++) {
