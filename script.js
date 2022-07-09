bf325088a8d96ed0431e22607b7dc7ea4ae05a45
soadaa <Soadhlh@gmail.com>
2022-07-09 13:33:15 -0400

    Operate works on first runthrough
    The calculator now works for intergers on the first runthrough. The operandButtons need debugging. After calculation, the sum becomes firstTerm but then operandButtons does not correctly add numbers to secondTerm.

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandElement = document.querySelector('[data-previous-operand]');
 const currentOperandElement = document.querySelector('[data-current-operand]');
 
-const equationArray = [];
-let operandPosistion;
+const numberArray = [];
+let operandChoice;
 let equationResult;
 let term = 0;
+let firstTerm;
+let secondTerm;
 
 // numberButtons
 for (i = 0; i < numberButtons.length; i++) {
@@ -17,62 +19,70 @@ for (i = 0; i < numberButtons.length; i++) {
         let currentlyClickedButton = e.target;
         let value = currentlyClickedButton.getAttribute('data-number');
         currentOperandElement.textContent += value;
-        term +=  value;
+        // Stops initial term value 0 being dragged along into numberArray
+        if (term === 0) {
+            term = value
+        } else {
+            term = 0
+            term += value
+        }
     });
 }
 
-// operands
+// operandButtons
 for (i = 0; i < operationButtons.length; i++) {
     operationButtons[i].addEventListener('click', function(e) {
         let currentlyClickedButton = e.target;
-        let operand = currentlyClickedButton.getAttribute('data-operation');
-        currentOperandElement.textContent += ' ' + operand + ' ';
-        equationArray.push(term);
-        console.log(equationArray);
-        operandPosistion = equationArray.indexOf(operand);
+        operandChoice = currentlyClickedButton.getAttribute('data-operation');
+        currentOperandElement.textContent += ' ' + operandChoice + ' ';
+        // store first term given, second term calculates equationResult
+        if (numberArray.length === 0) {
+            numberArray.push(term);
+            term = 0;
+        } else if (numberArray.length === 1) {
+            
+        }
     });
 }
 
 equalButton.addEventListener('click', () => {
-    operate();
+    numberArray.push(term);
+    operate(operandChoice);
     previousOperandElement.textContent = currentOperandElement.textContent + ' = ' + equationResult;
     currentOperandElement.textContent = equationResult;
-    equationArray.length = 0;
 });
 
 allClearButton.addEventListener('click', () => {
-    equationArray.length = 0;
-    operandPosistion = '';
+    numberArray.length = 0;
+    operandChoice = '';
     equationResult = '';
     previousOperandElement.textContent = '';
     currentOperandElement.textContent = '';
 });
 
 const operate = function() {
-    // Allows array to be sliced; Combines multiple numbers into a singular term
-    let expression = equationArray.toString().replace(/,/g,"");
-
-    // Seperates the two terms from the operator
-    firstTerm = parseInt(expression.slice(0, operandPosistion));
-    secondTerm = parseInt(expression.slice(operandPosistion + 1));
-    operand = expression.charAt(operandPosistion);
-    getEquationResult(operand);
+    firstTerm = parseInt(numberArray[0]);
+    secondTerm = parseInt(numberArray[1]);
+    getEquationResult(operandChoice);
+    numberArray.length = 0;
+    numberArray.push(equationResult);
+    term = equationResult;
 }
 
 
-const getEquationResult = function(operand) {
-    switch (operand) {
+const getEquationResult = function() {
+    switch (operandChoice) {
         case '+':
-            sum();
+            sum(firstTerm, secondTerm);
             break;
         case '-':
-            subtract();
+            subtract(firstTerm, secondTerm);
             break;
         case 'x' :
-            multiply();
+            multiply(firstTerm, secondTerm);
             break;
         case '/' :
-            divide();
+            divide(firstTerm, secondTerm);
             break;
         default:
             console.log('bug on switch statement')
@@ -80,28 +90,29 @@ const getEquationResult = function(operand) {
             break;
     }
 }
+
 const sum = function(firstTerm, secondTerm) {
-    total = firstTerm + secondTerm;
-    return total;
+    equationResult = firstTerm + secondTerm;
+    return equationResult;
 };
 
 const subtract = function(firstTerm, secondTerm) {
-    total = firstTerm - secondTerm;
-    return total;
+    equationResult = firstTerm - secondTerm;
+    return equationResult;
 };
 
 const multiply = function(firstTerm, secondTerm) {
-    total = firstTerm * secondTerm;
-    return total;
+    equationResult = firstTerm * secondTerm;
+    return equationResult;
 };
 
 const divide = function(firstTerm, secondTerm) {
-    total = firstTerm / secondTerm
-    return total;
+    equationResult = firstTerm / secondTerm
+    return equationResult;
 };
 
 const power = function(firstTerm, secondTerm) {
-	let total = Math.pow(firstTerm, secondTerm);
-  return total;
+	let equationResult = Math.pow(firstTerm, secondTerm);
+  return equationResult;
 };
 
